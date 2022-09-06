f13c41c4804d4622c5d9eba047093d278d779683
dAxpeDDa <daxpedda@gmail.com>
2022-09-06 12:02:39 +0200

    Fix wrong `init` usage

--- src/spawn.js
+++ src/spawn.js
@@ -1,7 +1,7 @@
 self.onmessage = async event => {
     let [module, memory, index] = event.data;
 
-    let wasm = await wasm_bindgen.initWithoutStart(module, memory);
+    let wasm = await wasm_bindgen(module, memory);
     wasm.test_2(index);
     wasm.__wbindgen_thread_destroy();
     self.close();
